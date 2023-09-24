a162590b58d52e5f7f8b261974adb498dae654bf
michealdemus <145695057+michealdemus@users.noreply.github.com>
2023-09-24 21:28:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ function onYouTubeIframeAPIReady() {
         height: '390',
         width: '640',
         videoId: 'dQw4w9WgXcQ', // Default video
+        playerVars: {
+            'autoplay': 1 // Autoplay the video
+        },
         events: {
             'onReady': onPlayerReady,
         }
@@ -13,12 +16,27 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
     event.target.playVideo();
+    // Attempt to make the video full screen; this may not work on some browsers.
+    try {
+        var iframe = document.getElementById('player');
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) { // Firefox
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) { // IE/Edge
+            iframe.msRequestFullscreen();
+        }
+    } catch (e) {
+        console.error("Unable to go full screen", e);
+    }
 }
 
 // Initialize Pusher
 // Replace APP_KEY and CLUSTER with your actual Pusher credentials
-const pusher = new Pusher('dd9c0093a77f8814a7bd', {
-    cluster: 'eu'
+const pusher = new Pusher('APP_KEY', {
+    cluster: 'CLUSTER'
 });
 
 // Subscribe to a channel
