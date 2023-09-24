cb415b797778e27b06ac194e5a9598168253f69f
michealdemus <145695057+michealdemus@users.noreply.github.com>
2023-09-24 19:20:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,4 @@
-// Zastąp YOUR_APP_ID rzeczywistym identyfikatorem aplikacji z Google Cast SDK
-const applicationID = 'B7FB11C3';
 
-// Inicjalizacja Google Cast SDK
-window['__onGCastApiAvailable'] = function(isAvailable) {
-    if (isAvailable) {
-        initializeCastApi();
-    }
-};
-
-function initializeCastApi() {
-    const sessionRequest = new chrome.cast.SessionRequest(applicationID);
-    const apiConfig = new chrome.cast.ApiConfig(
-        sessionRequest,
-        sessionListener,
-        receiverListener
-    );
-
-    chrome.cast.initialize(apiConfig, onInitSuccess, onError);
-}
-
-function onInitSuccess() {
-    console.log('Initialization succeeded');
-}
-
-function onError(message) {
-    console.log('Initialization failed: ' + message);
-}
-
-function sessionListener(session) {
-    console.log('Session started: ' + session);
-}
-
-function receiverListener(status) {
-    if (status === chrome.cast.ReceiverAvailability.AVAILABLE) {
-        document.getElementById('castButton').addEventListener('click', function() {
-            chrome.cast.requestSession(sessionListener, onError);
-        });
-    }
-}
 
 // Inicjalizacja Pusher
 // Zastąp APP_KEY i CLUSTER rzeczywistymi danymi z Twojego konta Pusher
