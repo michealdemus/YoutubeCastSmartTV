4092ef1f830b740136911dbf523ff0184c0a8cc9
michealdemus <145695057+michealdemus@users.noreply.github.com>
2023-09-25 11:11:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+
+// Existing JavaScript code
+// ...
+
+// Initialize Pusher with existing credentials
+const pusher = new Pusher('dd9c0093a77f8814a7bd', {
+    cluster: 'eu'
+});
+
+// Subscribe to a channel
+const channel = pusher.subscribe('my-channel');
+
+// Bind to an event within the channel
+channel.bind('new-video', function(data) {
+    // Update YouTube video based on the received ID
+    player.loadVideoById(data.videoId);
+    // Autoplay the video
+    player.playVideo();
+
+    // Update HTML element to display the current Pusher data
+    document.getElementById('pusherData').innerHTML = `Current Video ID: ${data.videoId}`;
+});
+
+// Functionality for Chromecast remote control
+document.addEventListener('keydown', function(event) {
+    let keyPressed = "Unknown Key";
+    switch(event.keyCode) {
+        case 34: // Enter for Play/Pause toggle
+            keyPressed = "Enter";
+            if (player.getPlayerState() === YT.PlayerState.PLAYING) {
+                player.pauseVideo();
+            } else {
+                player.playVideo();
+            }
+            break;
+        case 35: // Down Arrow for stopping the video
+            keyPressed = "Down Arrow";
+            player.stopVideo();
+            break;
+        case 36: // Left Arrow for Rewind
+            keyPressed = "Left Arrow";
+            var currentTime = player.getCurrentTime();
+            player.seekTo(currentTime - 10);
+            break;
+        case 37: // Up Arrow for playing the video
+            keyPressed = "Up Arrow";
+            player.playVideo();
+            break;
+        case 38: // Right Arrow for Fast Forward
+            keyPressed = "Right Arrow";
+            var currentTime = player.getCurrentTime();
+            player.seekTo(currentTime + 10);
+            break;
+        case 47: // Custom code for Volume Up
+            keyPressed = "Volume Up";
+            var currentVolume = player.getVolume();
+            player.setVolume(Math.min(currentVolume + 10, 100));
+            break;
+        case 48: // Custom code for Volume Down
+            keyPressed = "Volume Down";
+            var currentVolume = player.getVolume();
+            player.setVolume(Math.max(currentVolume - 10, 0));
+            break;
+        default:
+            keyPressed = `Other Key (${event.key})`; // Display the actual key name
+            break;
+    }
+    // Update HTML element to display the pressed key
+    document.getElementById('pusherData').innerHTML = `Last Key Pressed: ${keyPressed}`;
+});
+
+// Fetch next video ID using YouTube Data API (Pseudocode)
+// This is a placeholder as actual implementation would require server-side logic for API authentication
+async function fetchNextVideoID() {
+    // Use YouTube Data API to fetch recommended videos based on the current video ID
+    // Note: Actual API authentication and fetch logic would go here
+    const currentVideoID = player.getVideoData().video_id;
+    // Placeholder for fetched next video ID
+    const nextVideoID = "some_fetched_video_id";
+    return nextVideoID;
+}
+
+// Listen for video end event
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        // Fetch the next recommended video
+        fetchNextVideoID().then(nextVideoID => {
+            // Load and play the next video
+            player.loadVideoById(nextVideoID);
+        });
+    }
+}
